d8f1eec301ba76104ead3888ae1b6e88c518a7e2
aaronchn99 <36250963+aaronchn99@users.noreply.github.com>
2023-03-06 00:30:19 +0000

    Feat: Base markup, contact handler

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pui Chan</title>
+</head>
+<body>
+    <nav>
+        <a>About</a>
+        <a>Skills</a>
+        <a>Works</a>
+        <a>Contact</a>
+    </nav>
+    <article>
+        <h1 id="about">Pui Chan - Software Developer</h1>
+        <img src="" />
+        <p class="desc">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        
+        <h2 id="skills">Skills</h2>
+        <div class="skills">
+            
+        </div>
+        
+        <h2 id="works">Works</h2>
+        <div class="works">
+
+        </div>
+        
+        <h2 id="contact">Contact Me</h2>
+        <div class="contact">
+            <form method="post" action="/contact.php">
+                <input type="text" name="name" />
+                <input type="email" name="email" />
+                <input type="text" name="org" />
+                <textarea name="message"></textarea>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    </article>
+</body>
+</html>
