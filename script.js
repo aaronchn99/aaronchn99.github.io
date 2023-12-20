056fa562d3472181b2ecafd35267cb3b68faef77
aaronchn99 <36250963+aaronchn99@users.noreply.github.com>
2023-12-20 20:47:50 +0000

    Feat: - About me - Project Carousel

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-const scrollAtEarth = () => {
-    const earthRect = document.getElementsByClassName('earth')[0].getBoundingClientRect();
-    document.getElementsByTagName('body')[0].dataset.scrollEarth = window.scrollY <= earthRect.bottom;
+const docReady = f => {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(f, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", f);
+    }
 }
 
-scrollAtEarth();
-document.addEventListener('scroll', scrollAtEarth);
+docReady(() => {
+    const scrollAtEarth = () => {
+        const earthRect = document.getElementsByClassName('earth')[0].getBoundingClientRect();
+        document.getElementsByTagName('body')[0].dataset.scrollEarth = window.scrollY <= earthRect.bottom;
+    }
+    scrollAtEarth();
+    document.addEventListener('scroll', scrollAtEarth);
+    
+    window.addEventListener('resize', () => {
+        document.getElementsByClassName("entries")[0].style.height = `${document.getElementsByClassName("entry")[0].scrollHeight}px`
+    });
+    window.dispatchEvent(new Event('resize'));
+
+    const worksNavClick = (e) => {
+        const direction = e.currentTarget.classList[1];
+        const opposite = (direction == 'left') ? 'right' : 'left';
+        if (document.getElementsByClassName(`entry ${direction}`).length > 0) {
+            document.getElementsByClassName(`entry show`)[0].classList.replace('show', opposite);
+            const entryList = document.getElementsByClassName(`entry ${direction}`);
+            const entry = entryList[(direction == 'right')?0:entryList.length-1];
+            entry.classList.replace(direction, 'show');
+        }
+    };
+    for (let navButton of document.getElementsByClassName("nav")) {
+        navButton.addEventListener('click', worksNavClick, true);
+    }
+});
